49fd213c34fe350e6379057e12c86b12aca0bf84
SaiSaranyabevara <saranyabevara3@gmail.com>
2025-08-07 14:09:03 +0530

    first episode

---
+++ app.js
@@ -0,0 +1,15 @@
+ 
+            const heading=React.createElement("div",{id :"heading"},[
+                React.createElement("div",{id:"child1"},[
+                    React.createElement("h1",{},"this is h1"),
+                        React.createElement("h2",{},"this is h2")
+                    ]),
+                    React.createElement("div",{id:"child2"},[
+                        React.createElement("h1",{},"this is h1"),
+                        React.createElement("h2",{},"this is h2")
+                    ])
+                ]
+            );
+            const root=ReactDOM.createRoot(document.getElementById("root"));
+            root.render(heading);
+     
